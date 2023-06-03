e83ed7c8729ce6c28f0e2f561710bc589ea4bfc4
Antonio Maradiaga <antonio@maradia.ga>
2023-06-03 15:43:57 +0200

    Including service

--- btp/btpTypes.go
+++ btp/btpTypes.go
@@ -72,3 +72,99 @@ type DataCenter struct {
 	Domain                 string `json:"domain"`
 	GeoAccess              string `json:"geoAccess"`
 }
+
+type Service struct {
+	Name             string `json:"name"`
+	DisplayName      string `json:"displayName"`
+	Description      string `json:"description"`
+	BusinessCategory struct {
+		ID          string `json:"id"`
+		DisplayName string `json:"displayName"`
+	} `json:"businessCategory"`
+	OwnerType     string `json:"ownerType"`
+	TermsOfUseURL string `json:"termsOfUseUrl"`
+	ServicePlans  []struct {
+		Name                      string  `json:"name"`
+		DisplayName               string  `json:"displayName"`
+		Description               string  `json:"description"`
+		UniqueIdentifier          string  `json:"uniqueIdentifier"`
+		ProvisioningMethod        string  `json:"provisioningMethod"`
+		Amount                    float64 `json:"amount"`
+		RemainingAmount           float64 `json:"remainingAmount"`
+		ProvidedBy                string  `json:"providedBy"`
+		Beta                      bool    `json:"beta"`
+		AvailableForInternal      bool    `json:"availableForInternal"`
+		InternalQuotaLimit        any     `json:"internalQuotaLimit"`
+		AutoAssign                bool    `json:"autoAssign"`
+		AutoDistributeAmount      any     `json:"autoDistributeAmount"`
+		MaxAllowedSubaccountQuota any     `json:"maxAllowedSubaccountQuota"`
+		Category                  string  `json:"category"`
+		SourceEntitlements        []struct {
+			EntitlementName    string  `json:"entitlementName"`
+			Amount             float64 `json:"amount"`
+			ProductID          string  `json:"productId"`
+			Restricted         bool    `json:"restricted"`
+			AllowedSubaccounts any     `json:"allowedSubaccounts"`
+			CommercialModel    struct {
+				Name             string `json:"name"`
+				DisplayName      string `json:"displayName"`
+				ConsumptionBased bool   `json:"consumptionBased"`
+				Description      string `json:"description"`
+			} `json:"commercialModel"`
+			AutoAssign bool `json:"autoAssign"`
+		} `json:"sourceEntitlements"`
+		DataCenters []struct {
+			Name                   string `json:"name"`
+			DisplayName            string `json:"displayName"`
+			Region                 string `json:"region"`
+			Environment            string `json:"environment"`
+			IaasProvider           string `json:"iaasProvider"`
+			SupportsTrial          bool   `json:"supportsTrial"`
+			ProvisioningServiceURL string `json:"provisioningServiceUrl"`
+			SaasRegistryServiceURL string `json:"saasRegistryServiceUrl"`
+			Domain                 string `json:"domain"`
+			IsMainDataCenter       any    `json:"isMainDataCenter"`
+			GeoAccess              string `json:"geoAccess"`
+		} `json:"dataCenters"`
+		Resources              any   `json:"resources"`
+		DependsOn              any   `json:"dependsOn"`
+		AdditionalPlanFeatures []any `json:"additionalPlanFeatures"`
+		AssignedResources      any   `json:"assignedResources"`
+		Unlimited              bool  `json:"unlimited"`
+	} `json:"servicePlans"`
+	IconBase64             string `json:"iconBase64"`
+	ApplicationCoordinates struct {
+		IconFormat   string `json:"iconFormat"`
+		InventoryIds []struct {
+			Key string `json:"key"`
+		} `json:"inventoryIds"`
+		Visibility         string `json:"visibility"`
+		ServiceDescription []struct {
+			LinkCategory        string `json:"linkCategory"`
+			Title               string `json:"title"`
+			PropagateTheme      string `json:"propagateTheme"`
+			DescriptionCategory string `json:"descriptionCategory"`
+			LinkURL             string `json:"linkURL"`
+		} `json:"serviceDescription"`
+		ServiceCategories []struct {
+			Name string `json:"name"`
+		} `json:"serviceCategories"`
+		RegionInformation []struct {
+			Key string `json:"key"`
+		} `json:"regionInformation"`
+		CFService struct {
+			Name        string `json:"name"`
+			Description string `json:"description"`
+			Metadata    struct {
+				DisplayName string `json:"displayName"`
+			} `json:"metadata"`
+			Plans []struct {
+				TechnicalName string `json:"technicalName"`
+				Name          string `json:"name"`
+				Description   string `json:"description"`
+				Metadata      struct {
+				} `json:"metadata"`
+			} `json:"plans"`
+		} `json:"CFService"`
+	} `json:"applicationCoordinates"`
+}
