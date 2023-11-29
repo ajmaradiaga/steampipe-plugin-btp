5e61d523decbc75b036d70c648de2ae963b5f369
Antonio Maradiaga <antonio.maradiaga@sap.com>
2023-11-29 16:17:07 +0100

    Removed unnecessary fields

--- btp/connection_config.go
+++ btp/connection_config.go
@@ -6,53 +6,21 @@ import (
 )
 
 type BTPConfig struct {
-	// UAA
-	UAAAPIUrl       *string `cty:"uaa_apiurl"`
-	UAAClientId     *string `cty:"uaa_clientid"`
-	UAAClientSecret *string `cty:"uaa_clientsecret"`
-
 	// Endpoints
 	CISAccountServiceUrl      *string `cty:"cis_accounts_service_url"`
 	CISEntitlementsServiceUrl *string `cty:"cis_entitlements_service_url"`
 
 	// Username
-	Username    *string `cty:"username"`
-	Password    *string `cty:"password"`
 	AccessToken *string `cty:"cis_access_token"`
 }
 
 var ConfigSchema = map[string]*schema.Attribute{
-	"uaa_apiurl": {
-		Type:     schema.TypeString,
-		Required: false,
-	},
-
-	"uaa_clientid": {
-		Type:     schema.TypeString,
-		Required: false,
-	},
-
-	"uaa_clientsecret": {
-		Type:     schema.TypeString,
-		Required: false,
-	},
-
 	"cis_accounts_service_url": {
 		Type: schema.TypeString,
 	},
 
 	"cis_entitlements_service_url": {
 		Type: schema.TypeString,
-	},
-
-	"username": {
-		Type:     schema.TypeString,
-		Required: false,
-	},
-
-	"password": {
-		Type:     schema.TypeString,
-		Required: false,
 	},
 
 	"cis_access_token": {
