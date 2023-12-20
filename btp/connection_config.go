d6717e6a57d73caa4e64ddc7a592ccecf0a21a96
Antonio Maradiaga <antonio@maradia.ga>
2023-12-20 11:20:16 -0600

    Removed config schema

--- btp/connection_config.go
+++ btp/connection_config.go
@@ -2,27 +2,12 @@ package btp
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type BTPConfig struct {
 	CISAccountServiceUrl      *string `hcl:"cis_accounts_service_url"`
 	CISEntitlementsServiceUrl *string `hcl:"cis_entitlements_service_url"`
 	AccessToken               *string `hcl:"cis_access_token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"cis_accounts_service_url": {
-		Type: schema.TypeString,
-	},
-
-	"cis_entitlements_service_url": {
-		Type: schema.TypeString,
-	},
-
-	"cis_access_token": {
-		Type: schema.TypeString,
-	},
 }
 
 func ConfigInstance() interface{} {
